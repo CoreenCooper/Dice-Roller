b5c38460252c9cdeeb5e9a0f1bc75d4484cd89e2
Coreen Cooper <coreencooper@pursuit.org>
2021-02-28 11:48:59 -0500

    display sum

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const numOfDice = document.querySelector("#num-of-dice"); // grab user input
 const rollDiceBtn = document.querySelector("#roll-dice"); // grab button
 const displayDice = document.querySelector("#dice-para"); // display roll result
+const displaySum = document.querySelector("#sum-para"); // display sum result
 let rollResult = "";
+let sum = 0;
 
 const diceRoll = (event) => {
     event.preventDefault();
     rollResult = "";
+    sum = 0;
     const userInput = Number(numOfDice.value);
     for(let i = 0; i < userInput; i++) {
         const randNum = Math.floor(Math.random() * 6);
         rollResult += randNum;
+        sum += randNum;
     }
-displayDice.textContent = rollResult;
+displayDice.textContent = `${rollResult}`;
+displaySum.textContent = `Sum = ${sum}`;
 }
 
 
