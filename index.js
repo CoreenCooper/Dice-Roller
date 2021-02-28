7f64a64082e1fcdb129dc0f66ef8cdbc8fcefb5c
Coreen Cooper <coreencooper@pursuit.org>
2021-02-28 13:07:49 -0500

    add dice[]

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const dice = [
+    { die:"&#9856", value: 1},
+    { die:"&#9857", value: 2},
+    { die:"&#9858", value: 3},
+    { die:"&#9859", value: 4},
+    { die:"&#9860", value: 5},
+    { die:"&#9861", value: 6},
+]
 const numOfDice = document.querySelector("#num-of-dice"); // grab user input
 const rollDiceBtn = document.querySelector("#roll-dice"); // grab button
 const displayDice = document.querySelector("#dice-para"); // display roll result
@@ -31,3 +39,4 @@ const history = () => {
 }
 
 rollDiceBtn.addEventListener("submit", diceRoller);
+
